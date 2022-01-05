34c987ce31c2762fd1fde59f8bbef58fbb06ddad
satnamkhowal <satnamkhowal@gmail.com>
2022-01-05 14:53:38 +0530

    First Commit for Groot Academy Website

---
+++ includes/footer.php
@@ -0,0 +1,157 @@
+       <!-- Footer Start -->
+       <footer id="rs-footer" class="rs-footer">
+            <div class="footer-top">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-12 col-sm-12 footer-widget">
+                            <div class="footer-logo mb-30">
+                                <a href="index.php"><img src="assets/images/logo-dark.png" alt=""></a>
+                            </div>
+                              <div class="textwidget pb-30"><p>Sedut perspiciatis unde omnis iste natus error sitlutem acc usantium doloremque denounce with illo inventore veritatis</p>
+                              </div>
+                              <ul class="footer-social md-mb-30">  
+                                  <li><a href="https://www.facebook.com/GrootSoftware/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                                            <li><a href="https://twitter.com/grootsoftwa" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                                            <li><a href="https://github.com/grootsoftware" target="_blank"><i class="fa fa-git"></i></a></li>
+                                            <li><a href="https://www.instagram.com/grootsoftware/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+                                            <li><a href="https://www.linkedin.com/in/grootsoftware/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+                                            <li><a href="https://stackoverflow.com/c/grootsoftware/" target="_blank"><i class="fa fa-stack-overflow"></i></a></li>                                                         
+                              </ul>
+                        </div>
+                        <div class="col-lg-3 col-md-12 col-sm-12 pl-45 md-pl-15 md-mb-30">
+                            <h3 class="widget-title">IT Services</h3>
+                            <ul class="site-map">
+                                <li><a href="software-development.php">Software Development</a></li>
+                                <li><a href="web-development.php">Web Development</a></li>
+                                <li><a href="analytic-solutions.php">Analytic Solutions</a></li>
+                                <li><a href="web-development.php">Cloud and DevOps</a></li>
+                                <li><a href="web-development.php">Product Design</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-3 col-md-12 col-sm-12 md-mb-30">
+                            <h3 class="widget-title">Contact Info</h3>
+                            <ul class="address-widget">
+                                <li>
+                                    <i class="flaticon-location"></i>
+                                    <div class="desc"> 48, Sultan Nagar, Jaipur, Rajasthan 302019 </div>
+                                </li>
+                                <li>
+                                    <i class="flaticon-call"></i>
+                                    <div class="desc">
+                                       <a href="tel:+917790847091">+91-7790847091</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <i class="flaticon-email"></i>
+                                    <div class="desc">
+                                        <a href="mailto:info@grootsoftware.com">info@grootsoftware.com</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <i class="flaticon-clock-1"></i>
+                                    <div class="desc">
+                                        Opening Hours: 8:00AM - 9:00PM   
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-3 col-md-12 col-sm-12">
+                            <h3 class="widget-title">Newsletter</h3>
+                            <p class="widget-desc">We denounce with righteous and in and dislike men who are so beguiled and demo realized.</p>
+                            <p>
+                                <input type="email" name="EMAIL" placeholder="Your email address" required="">
+                                <em class="paper-plane"><input type="submit" value="Sign up"></em>
+                                <i class="flaticon-send"></i>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <div class="container">                    
+                    <div class="row y-middle">
+                        <div class="col-lg-6 text-right md-mb-10 order-last">
+                            <ul class="copy-right-menu">
+                                <li><a href="index.php">Home</a></li>
+                                <li><a href="about.php">About</a></li>
+                                <li><a href="blog.php">Blog</a></li>
+                                <li><a href="shop.php">Shop</a></li>
+                                <li><a href="faq.php">FAQs</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="copyright">
+                                <p>&copy; 2021 All Rights Reserved. Developed By <a href="http://grootsoftware.com/">GrootSoftware Pvt. Ltd.</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Footer End -->
+
+        <!-- start scrollUp  -->
+        <div id="scrollUp" class="orange-color">
+            <i class="fa fa-angle-up"></i>
+        </div>
+        <!-- End scrollUp  -->
+
+        <!-- Search Modal Start -->
+        <div aria-hidden="true" class="modal fade search-modal" role="dialog" tabindex="-1">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span class="flaticon-cross"></span>
+            </button>
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="search-block clearfix">
+                        <form>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Search Here..." type="text">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Search Modal End -->
+
+         <!-- modernizr js -->
+         <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+         <script src="assets/js/script.js"></script>
+        <!-- jquery latest version -->
+        <script src="assets/js/jquery.min.js"></script>
+        <!-- Bootstrap v4.4.1 js -->
+        <script src="assets/js/bootstrap.min.js"></script>
+        <!-- Menu js -->
+        <!-- <script src="assets/js/rsmenu-main.js"></script>  -->
+        <!-- op nav js -->
+        <script src="assets/js/jquery.nav.js"></script>
+        <!-- Time Circle js -->
+        <script src="assets/js/time-circle.js"></script>
+        <!-- owl.carousel js -->
+        <script src="assets/js/owl.carousel.min.js"></script>
+        <!-- wow js -->
+        <script src="assets/js/wow.min.js"></script>
+        <!-- Skill bar js -->
+        <script src="assets/js/skill.bars.jquery.js"></script>
+        <script src="assets/js/jquery.counterup.min.js"></script> 
+         <!-- counter top js -->
+        <script src="assets/js/waypoints.min.js"></script>
+        <!-- swiper js -->
+        <script src="assets/js/swiper.min.js"></script>   
+        <!-- particles js -->
+        <script src="assets/js/particles.min.js"></script>  
+        <!-- magnific popup js -->
+        <script src="assets/js/jquery.magnific-popup.min.js"></script>      
+        <script src="assets/js/jquery.easypiechart.min.js"></script>      
+        <!-- plugins js -->
+        <script src="assets/js/plugins.js"></script>
+        <!-- pointer js -->
+        <script src="assets/js/pointer.js"></script>
+        <!-- contact form js -->
+        <script src="assets/js/contact.form.js"></script>
+        <!-- main js -->
+        <script src="assets/js/main.js"></script>
+    </body>
+
+</html>
