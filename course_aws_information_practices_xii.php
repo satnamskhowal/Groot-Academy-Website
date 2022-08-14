3504ca89b92c8b8a8f22a6f24a023babd93a7038
satnamkhowal <satnamkhowal@gmail.com>
2022-08-14 16:04:27 +0530

    slkfj

---
+++ course_aws_information_practices_xii.php
@@ -0,0 +1,53 @@
+<?php include 'includes/header.php';?>
+
+
+<!-- course details starts -->
+            <div class="rs-services style4 modify1 gray-color pt-110 pb-120 md-pt-75 md-pb-40 sm-pb-70">
+                <div class="container">
+                    <div class="sec-title2 text-center mb-45">
+                        <h2 class="title">
+                          Top Populer Courses
+                        </h2>
+                    </div>
+                    <div class="d-flex flex-column align-items-center">
+                        
+                                <div style="width:300px">
+                                    <h5 class=""><a href="web-development.php">Information practices XII</a></h5>
+                                    
+                                </div>
+
+                                <div style="font-size:14px;color:gray;width:300px">
+                                    <div>
+                                        <ul>
+
+                                            <li>Training Duration: 3 Months</li>
+                                            <li>Training Fees: ₹000000</li>
+                                        </ul>
+                                    </div>
+
+                                    <hr height="5px">
+
+                                    <div>
+                                        <ul class="pl-4" style="list-style-type:circle">
+                                            <li>demo</li>
+                                            <li>demo</li>
+                                            <li>demo</li>
+                                            <li>demo</li>
+                                            <li>demo</li>
+                                            <li>demo</li>
+                                            <li>demo</li>
+                                        </ul>
+                                    </div>
+
+                                </div>
+                   
+                        
+                    </div>
+                </div>
+            </div>
+<!-- course details ends -->
+
+
+
+<?php include 'includes/footer.php';?>
+ 
---
+++ courses.php
@@ -0,0 +1,128 @@
+<?php include 'includes/header.php';?>
+
+
+
+
+
+
+<!-- Courses section starts-->
+            <div class="rs-services style4 modify1 gray-color pt-110 pb-120 md-pt-75 md-pb-40 sm-pb-70">
+                <div class="container">
+                    <div class="sec-title2 text-center mb-45">
+                        <span class="sub-text">Our Latest courses</span>
+                        <h2 class="title">
+                          Top Populer Courses
+                        </h2>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4 mb-20">
+                            <div class="services-item">
+                                <div class="services-icon">
+                                    <img src="assets/images/services/style5/1.png" alt="">
+                                </div>
+                                <div class="services-content">
+                                    <h2 class="title"><a href="course_aws.php">AWS</a></h2>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-20">
+                            <div class="services-item">
+                                <div class="services-icon">
+                                    <img src="assets/images/services/style5/2.png" alt="">
+                                </div>
+                                <div class="services-content">
+                                    <h2 class="title"><a href="course_salesforce_admin(online).php">Salesforce Admin ( Online )</a></h2>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-20">
+                            <div class="services-item">
+                                <div class="services-icon">
+                                    <img src="assets/images/services/style5/3.png" alt="">
+                                </div>
+                                <div class="services-content">
+                                    <h2 class="title"><a href="course_aws_information_practices_xi.php">Information practices XI</a></h2>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="services-item">
+                                <div class="services-icon">
+                                    <img src="assets/images/services/style5/4.png" alt="">
+                                </div>
+                                <div class="services-content">
+                                    <h2 class="title"><a href="course_aws_information_practices_xii.php">Information practices XII</a></h2>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="services-item">
+                                <div class="services-icon">
+                                    <img src="assets/images/services/style5/5.png" alt="">
+                                </div>
+                                <div class="services-content">
+                                    <h2 class="title"><a href="courses_machine_learning.php">Machine Learning</a></h2>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="services-item">
+                                <div class="services-icon">
+                                    <img src="assets/images/services/style5/6.png" alt="">
+                                </div>
+                                <div class="services-content">
+                                    <h2 class="title"><a href="course_core_java.php">Core Java</a></h2>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="services-item">
+                                <div class="services-icon">
+                                    <img src="assets/images/services/style5/6.png" alt="">
+                                </div>
+                                <div class="services-content">
+                                    <h2 class="title"><a href="course_advance_java.php">Advance Java</a></h2>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="services-item">
+                                <div class="services-icon">
+                                    <img src="assets/images/services/style5/6.png" alt="">
+                                </div>
+                                <div class="services-content">
+                                    <h2 class="title"><a href="course_salesforce_admin_&_app_builder.php">Salesforce Admin & App Builder</a></h2>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="services-item">
+                                <div class="services-icon">
+                                    <img src="assets/images/services/style5/6.png" alt="">
+                                </div>
+                                <div class="services-content">
+                                    <h2 class="title"><a href="course_salesforce_developer.php">Salesforce Developer</a></h2>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+<!-- Courses section ends-->
+
+
+
+
+
+
+<?php include 'includes/footer.php';?>
+ 
